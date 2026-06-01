04d039e8859393a4acc7d3a13419d9c94edff5d3
MayurThorat319 <mayurthorat319@gmail.com>
2026-06-01 19:25:38 +0530

    new sec added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import Hero from "./components/MainSection/Hero";
 import WayUsSection from "./components/MainSection/WayUs";
-import AboutSection from "./components/MainSection/About";
+// import AboutSection from "./components/MainSection/About";
 import PerfectMove from "./components/MainSection/PerfectMove";
 import Slider from "./components/MainSection/slider";
 // import FaqSection from "./components/MainSection/faqs";
